f59f4241b5eee5801e0d24e8c68fb2df315a53a0
J <80164315+jbolns@users.noreply.github.com>
2023-12-22 15:23:09 +0200

    GH pages shenanigans 3

--- utils/js/mainjs.js
+++ utils/js/mainjs.js
@@ -21,7 +21,7 @@ async function load() {
   console.log('pre is... ', pre)
 
   // Get/write body elements: <header>, <main>, <footer>
-  const header = await $.get(`./components/header.html`, function (data) {
+  const header = await $.get(`./weird_website/components/header.html`, function (data) {
     return data
   })
 
